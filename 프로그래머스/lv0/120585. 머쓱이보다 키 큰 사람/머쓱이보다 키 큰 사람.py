a1ff05fdb9a425ae5e04dee070b0a2739a8ac235
Shoon2 <117843786+Dawnnote@users.noreply.github.com>
2023-02-15 21:15:29 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -0,0 +1,9 @@
+def solution(array, height):
+    result = 0
+    for i in range(len(array)):
+        if array[i] > height:
+            result += 1
+        else:
+            result
+            
+    return result
