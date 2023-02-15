d8b0dbcbdc376e501f7f07b6d710d2bf4e65bb90
Shoon2 <117843786+Dawnnote@users.noreply.github.com>
2023-02-15 21:22:06 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    result = ""
+    for i in my_string:
+        if i.isupper():
+            result += i.lower()
+        else:
+            result += i.upper()
+    return result
