eaf349be3089370986ecdee0319bcbc8356a410c
Dawnnote <lshoon7454@gmail.com>
2023-02-16 12:48:37 +0900

    [Silver III] Title: 후위 표기식2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
@@ -5,10 +5,10 @@ num = [int(input()) for i in range(N)]
 stack = []
 
 for i in operator:
-    if i.isalpha():
-        stack.append(num[ord(i)-ord('A')])
-    else:
-        op2 = stack.pop()
+    if i.isalpha():                             # 문자 일때
+        stack.append(num[ord(i)-ord('A')])      # ord는 문자를 유니코드 정수로 바꾸어준다
+    else:                                       # 문자가 아닌 연산자를 기호를 만날 때
+        op2 = stack.pop()                       # 해당 숫자(유니코드)가 담긴 stack에 pop을 해서 op2, op1, 변수에 담아준다
         op1 = stack.pop()
 
         if i == '*':
