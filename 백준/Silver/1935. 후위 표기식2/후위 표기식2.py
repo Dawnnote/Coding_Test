f772206725e2777c5842b263b8078f3c5209d22d
Shoon2 <117843786+Dawnnote@users.noreply.github.com>
2023-02-16 12:34:19 +0900

    [Silver III] Title: 후위 표기식2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
@@ -0,0 +1,23 @@
+N = int(input())
+operator = input()
+num = [int(input()) for i in range(N)]
+
+stack = []
+
+for i in operator:
+    if i.isalpha():
+        stack.append(num[ord(i)-ord('A')])
+    else:
+        op2 = stack.pop()
+        op1 = stack.pop()
+
+        if i == '*':
+            stack.append(op1*op2)
+        elif i == '+':
+            stack.append(op1+op2)
+        elif i == '/':
+            stack.append(op1/op2)
+        elif i == '-':
+            stack.append(op1-op2)
+
+print("%.2f" %stack[0])
