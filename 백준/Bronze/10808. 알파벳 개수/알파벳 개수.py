638927acd6ad6b48055d77ec9330bc361e590763
Shoon2 <117843786+Dawnnote@users.noreply.github.com>
2023-02-17 09:22:35 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -1,11 +1,11 @@
 S = input()
 
-abc = {'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0,
-       'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0,
-       'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0}
+abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+       'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+num = [0]*26
 
-for i in S:
-    abc[i] += 1
-    
-for value in abc.values():
-    print(value, end=" ")
+for i in range(len(S)):
+    if S[i] in abc:
+        num[abc.index(S[i])] += 1
+
+print(" ".join(map(str, num)))
